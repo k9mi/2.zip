1609f198e59c306e7922f7f670814e5e36b9e0ec
k9mi <89679159+k9mi@users.noreply.github.com>
2021-09-05 07:55:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,29 +14,13 @@ wget "https://github.com/k9mi/2.zip/blob/main/2.zip"
 lua start.lua
 }
 installall(){
-apt update
-apt upgrade
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install tmux
-sudo apt-get install luarocks
-sudo apt-get install screen
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-sudo apt-get update
-sudo apt-get install
-sudo apt-get install upstart-sysv
-wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz;tar zxpf luarocks-2.2.2.tar.gz;cd luarocks-2.2.2 && ./configure
-sudo make bootstrap
-sudo luarocks install luasocket
-sudo luarocks install luasec
-sudo apt-get install libconfig++9v5 -y 
-sudo apt-get install libstdc++6 -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
-sudo apt-get install lua-lgi -y  
-sudo apt-get install libnotify-dev -y 
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
-sudo apt-get update 
-sudo apt-get upgrade -y
+pkg install phppkg install php
+pkg install php
+pkg install unzip
+unzip 2.zip
+pkg install screen
+php bot.php
+
 }
 if [ "$1" = "ins" ]; then
 install
